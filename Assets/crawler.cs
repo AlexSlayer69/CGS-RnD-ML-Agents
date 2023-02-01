4d55ffe0e5c2a268cca9aed137f7315da6fe153a
DrGabeMoriarty <gabrielabraham2003@gmail.com>
2023-02-01 11:16:07 +0530

    applied machine learning to crawler
    started training the quadrupedal crawler to stand with stability and move

--- Assets/crawler.cs
+++ Assets/crawler.cs
@@ -9,8 +9,12 @@ public class crawler : Agent
 {
     Rigidbody rbd;
     public float g=1,f=1;
-    public Transform bodyTransform;
+    public Transform bodyTransform,targetTransform;
     public Joint[] joints;
+    public float jointTorque;
+    public GameObject target;
+    public Collider bodyCollider;
+    public float energyUsage;
 
     enum actionState { 
         idle,moveForward,moveBack,moveLeft,moveRight,turnLeft,turnRight
@@ -23,38 +27,74 @@ public class crawler : Agent
         sensor.AddObservation(bodyTransform.localPosition.y);
         sensor.AddObservation(bodyTransform.localPosition.x);
         sensor.AddObservation(bodyTransform.localPosition.z);
-        sensor.AddObservation(bodyTransform.localEulerAngles.y);
-        sensor.AddObservation(bodyTransform.localEulerAngles.x);
-        sensor.AddObservation(bodyTransform.localEulerAngles.z);
+
+        sensor.AddObservation(targetTransform.localPosition.y);
+        sensor.AddObservation(targetTransform.localPosition.x);
+        sensor.AddObservation(targetTransform.localPosition.z);
+
+        //sensor.AddObservation(bodyTransform.localEulerAngles.y);
+        //sensor.AddObservation(bodyTransform.localEulerAngles.x);
+        //sensor.AddObservation(bodyTransform.localEulerAngles.z);
         
     }
 
     public override void OnEpisodeBegin()
     {
-        base.OnEpisodeBegin();
-
         //sets a random action state at the start of the episode
         System.Array A = System.Enum.GetValues(typeof(actionState));
         action = (actionState)A.GetValue(UnityEngine.Random.Range(0, A.Length));
+
+        target.transform.localPosition = new Vector3(Random.Range(-20, 20),2, Random.Range(-20, 20));
+        bodyTransform.localPosition = new Vector3(0, 5, 0);
+        bodyTransform.rotation = Quaternion.identity;
+        foreach (Joint joint in joints)
+        {
+            joint.transform.rotation = Quaternion.identity;
+        }
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        
+        for (int i = 0; i < 4; i++)
+        {
+            joints[i].transform.eulerAngles += new Vector3(actions.ContinuousActions[3*i], actions.ContinuousActions[3 * i+1], actions.ContinuousActions[3 * i+2])*jointTorque*Time.fixedDeltaTime;
+        }
+
+        for (int i = 4; i < joints.Length; i++)
+        {
+            joints[i].transform.eulerAngles += new Vector3(actions.ContinuousActions[i+8], actions.ContinuousActions[i + 8], actions.ContinuousActions[i + 8]) * jointTorque*Time.fixedDeltaTime;
+        }
+
+        //for (int i = 0; i < actions.ContinuousActions.Length; i++)
+        //{
+            //AddReward(-1 * energyUsage * actions.ContinuousActions[i] * actions.ContinuousActions[i]);
+        //}
+
+        Vector2 relativePos = new Vector2(bodyTransform.position.x - targetTransform.position.x, bodyTransform.position.z - targetTransform.position.z);
+        if (relativePos.sqrMagnitude<0.25f)
+        {
+            SetReward(1 / Time.fixedDeltaTime);
+            EndEpisode();
+        }
+
+        if(bodyTransform.localPosition.x>=25 || bodyTransform.localPosition.x <=-25 || bodyTransform.localPosition.y >= 25 || bodyTransform.localPosition.y <= -25 || bodyTransform.localPosition.y <= 3f)
+        {
+            SetReward(-1 / Time.fixedDeltaTime);
+            EndEpisode();
+        }
+
     }
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
-        discreteActions[0] = (int)Input.GetAxis("Horizontal");
-        discreteActions[1] = (int)Input.GetAxis("Vertical");
-        discreteActions[2] = Input.GetKey(KeyCode.E) ? 1 : Input.GetKey(KeyCode.Q) ? -1 : 0;
-
+        float moveX, moveY;
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        target.transform.Translate(moveX * Time.deltaTime, 0, moveY * Time.deltaTime);
     }
     void Start()
     {
         rbd = GetComponent<Rigidbody>();
-
-        
+        targetTransform = target.GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -64,4 +104,6 @@ public class crawler : Agent
             rbd.AddForce(0,1 * f, 0);
         rbd.AddForce(f/5*Input.GetAxis("Horizontal"), -1*g, f/5*Input.GetAxis("Vertical"));
     }
+
+   
 }
